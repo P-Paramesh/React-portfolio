13be101e5ff486d516a83f0f055714affed3441b
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-01-04 10:30:49 +0530

    Create Header.js

---
+++ pages/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  chakra,
+  HStack,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  Box,
+  Flex,
+  useColorModeValue,
+  useDisclosure,
+  VStack,
+  Button,
+  SimpleGrid,
+  Stack,
+  Icon,
+} from "@chakra-ui/react";
+import { useViewportScroll } from "framer-motion";
