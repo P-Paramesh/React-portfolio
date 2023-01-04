8e3a360ce8e9dc7f47d8779321a286b188c3e104
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-01-04 10:31:38 +0530

    Update Header.js

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -17,3 +17,9 @@ import {
   Icon,
 } from "@chakra-ui/react";
 import { useViewportScroll } from "framer-motion";
+
+export default function Header() {
+  const bg = useColorModeValue("white", "white");
+  const ref = React.useRef();
+  const [y, setY] = React.useState(0);
+  const { height = 0 } = ref.current ? ref.current.getBoundingClientRect() : {};
