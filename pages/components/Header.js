3454bcf048027993ab4fa6904f9b902d975cc331
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-01-05 20:33:42 +0530

    Update Header.js

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -16,6 +16,7 @@ import {
   Stack,
   Icon,
 } from "@chakra-ui/react";
+
 import { useViewportScroll } from "framer-motion";
 
 export default function Header() {
@@ -30,3 +31,9 @@ export default function Header() {
   }, [scrollY]);
   const cl = useColorModeValue("gray.800", "white");
   const mobileNav = useDisclosure();
+
+  const Section = (props) => {
+  const ic = useColorModeValue("brand.600", "brand.50");
+  const hbg = useColorModeValue("white", "brand.400");
+  const tcl = useColorModeValue("gray.900", "gray.50");
+  const dcl = useColorModeValue("gray.500", "gray.50");
