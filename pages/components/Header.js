61520ccd5c232be3ae25026b93d8acacbb00ba6a
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-01-08 22:25:35 +0530

    Update Header.js

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -136,3 +136,8 @@ const Features = (props) => {
                   strokeLinejoin="round"
                   strokeWidth="2"
                 >
+                    <path d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                  <path d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </Icon>
+              </Link>
+            </Box>
