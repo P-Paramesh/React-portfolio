5f80d66cdcaa3e292fb4cef6d8ea75f5ea576c5b
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-01-09 20:41:41 +0530

    Update Header.js

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -175,3 +175,22 @@ const Features = (props) => {
       </React.Fragment>
     );
   };
+  
+  const MobileNavContent = (
+    <VStack
+      pos="absolute"
+      top={0}
+      left={0}
+      right={0}
+      display={mobileNav.isOpen ? "flex" : "none"}
+      flexDirection="column"
+      p={2}
+      pb={4}
+      m={2}
+      bg={bg}
+      spacing={3}
+      rounded="sm"
+      shadow="sm"
+    >
+    </VStack>
+  );
