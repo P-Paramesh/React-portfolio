4ab7e14cb7c595a80bd84dcaabfa235d55d0c28d
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-06-22 13:52:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
-import './App.css';
+import { IconButton } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { VStack, Heading, Flex, Spacer} from "@chakra-ui/layout";
+import { FaLinkedin} from "react-icons/fa";
+const App = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+    const isDark = colorMode === "dark"
 
-function App() {
-  return (
-  );
-}
-
+    return (
+      <VStack p="5">
+        <Flex w="100%">
+          <Heading ml="8" size="md" fontWeight="semibold" color="cyan.400">
+            Paramesh P
+          </Heading>
+          <Spacer />
+          <IconButton icon={<FaLinkedin />}
+          isRound="true"
+          onClick={() =>
+          window.open("https://www.linkedin.com/in/paramesh-p-3603321ba/")}></IconButton>
+        </Flex>
+      </VStack>
+    )
 export default App;
