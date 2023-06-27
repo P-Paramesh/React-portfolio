c05f3693dacc58e38e1ca93e03bcf79a3f98061d
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-06-27 21:46:02 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import { Button } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Image } from "@chakra-ui/image";
+import { Circle, Stack, Flex, Box, Text } from "@chakra-ui/layout";
+import { useMediaQuery } from "@chakra-ui/media-query";
+import React from "react";
 
 const Header = () => {
-  return (
-   
-  )
-}
+  const { colorMode } = useColorMode();
+  const isDark = colorMode === "dark";
 
-export default Header
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+  return (
+    <Stack>
+      {isNotSmallerScreen && (
+        <Circle
+          position="absolute"
+          bg="blue.100"
+          opacity="0.1"
+          w="300px"
+          h="300px"
+          alignSelf="flex-end"
+        />
+      )}
+      <Flex
+        direction={isNotSmallerScreen ? "row" : "column"}
+        spacing="200px"
+        p={isNotSmallerScreen ? "32" : 0}
+        alignSelf="flex-start"
+      >
+        <Box mt={isNotSmallerScreen ? 0 : 16} align="flex-start">
+          <Text fontSize="5xl" fontWeight="semibold">
+            Hi, I am
+          </Text>
+          <Text
+            fontSize="7xl"
+            fontWeight="bold"
+            bgGradient="linear(to-r, cyan.400, blue.500, purple.600)"
+            bgClip="text"
+          >
+            Paramesh
+          </Text>
+          <Text color={isDark ? "gray.200" : "gray.500"}>
+           I am a Frontend Developer love to develope Web Applications
+          </Text>
+          <Button mt={8} colorScheme="blue">
+            Hire Me
+          </Button>
+        </Box>
+        <Image
+          alignSelf="center"
+          mt={isNotSmallerScreen ? "0" : "12"}
+          mb={isNotSmallerScreen ? "0" : "12"}
+          borderRadius="full"
+          backgroundColor="transparent"
+          boxShadow="lg"
+          boxSize="300px"
+          src='https://avatars.githubusercontent.com/u/95079545?v=4'
+          zIndex={1}
+/>
+      </Flex>
+    </Stack>
+  );
+};
+
+export default Header;
