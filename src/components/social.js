e1f4d2e0c15d0cc278d4e0b9060f1d2b4515f884
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-07-10 13:20:59 +0530

    Update social.js

--- src/components/social.js
+++ src/components/social.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import Icon from "@chakra-ui/icon";
+import { HStack } from "@chakra-ui/react";
+import React from "react";
+import {FaYoutube, FaGithub, FaLinkedin} from "react-icons/fa";
 
-const social = () => {
+
+const Social = () => {
   return (
-    <div>social</div>
+    <HStack spacing={24}>
+      <Icon as={FaGithub} boxSize={50} />
+      <Icon as={FaLinkedin} boxSize={50} />
+      <Icon as={FaYoutube} boxSize={50} />
+    </HStack>
   )
 }
 
-export default social
+export default Social;
