f5d1b75c520be27a4d6f055b454e7bead6af0236
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-08-16 10:28:08 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useMediaQuery } from '@chakra-ui/media-query';
-import {Box, Flex,Heading,Text, }from "@chakra-ui/layout";
+import {Box, Flex,Heading,Text, }from "@chakra-ui/layout"
 import Icon from "@chakra-ui/icon";
 import {DiReact, } from "react-icons/di";
 import {SiChakraui} from "react-icons/si";
@@ -9,31 +9,58 @@ import {ImGithub} from "react-icons/im";
 const Profile = () => {
   const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
   return(
-    <Flex direction={isNotSmallerScreen ? "row" : "column"} w="100%" maxWidth={{base:"100vh",md:"130vh", lg:"130vh", xl:"130vh"}}>
+    <Flex direction={isNotSmallerScreen ? "row" : "column"} w="100%" 
+    maxWidth={{base:"100vh",
+    md:"130vh", 
+    lg:"130vh", 
+    xl:"130vh"}}>
       <Box alignSelf="center" px="32" py="16">
-        <Heading fontWeight="extrabold" color="cyan.500" size="3xl">5+</Heading>
-        <Text fontSize="2xl" color="gray.500" padding="3px">Months <br /> Experience</Text>
+        <Heading fontWeight="extrabold" color="cyan.500" size="4xl">
+          5+
+        </Heading>
+        <Text fontSize="2xl" 
+        color="gray.500" >Months <br /> Experience
+        </Text>
       </Box>
       <Box alignSelf="center" px="32" py="16">
-        <Text fontWeight="2xl">Product Specialist and Developer, specialise in web app development</Text>
+        <Text fontWeight="bold">Product Specialist and Developer, specialise in web app development.
+        </Text>
         <Flex direction={isNotSmallerScreen ? "row" : "column"} mt={8}>
-          <Flex rounded="xl" direction="column" m={6} bg="blue.800" h="30vh" w="30vh" justify="flex-end">
-            <Icon color="white" p="4" w="24" h="24" as={DiReact} />
-           <Text color="gray.800" p="4" fontSize="xl" fontWeight="semibold">
-              React
-            </Text>
+          <Flex rounded="xl" 
+          direction="column" mt={4} 
+          bg="blue.800" 
+          h="30vh" 
+          w="30vh" 
+          justify="flex-end"
+          >
+          <Icon color="white" p="4" w="24" h="24" as={DiReact} />
+          <Text color="white" p="4" fontSize="xl" fontWeight="semibold">
+            React
+          </Text>
           </Flex>
-         <Flex rounded="xl" direction="column" m={6} bg="blue.300"_hover={{bg:"blue.400"}}  h="30vh" w="30vh" justify="flex-end">
-            <Icon color="white" p="4" w="24" h="24" as={SiChakraui} />
-            <Text color="gray.800" p="4" fontSize="xl" fontWeight="semibold">
-             Chakra Ui
-            </Text>
+         <Flex rounded="xl" 
+         direction="column" 
+         mt={4} 
+         bg="blue.300"
+         justify="flex-end"
+         ml={isNotSmallerScreen ? 4:0}
+         _hover={{bg:"blue.400"}}  h="30vh" w="30vh" >
+          <Icon color="white" p="4" w="24" h="24" as={SiChakraui} />
+          <Text color="white" p="4" fontSize="xl" fontWeight="semibold">
+           Chakra Ui
+          </Text>
           </Flex>
-          <Flex rounded="xl" direction="column" m={6} bg="teal.300"_hover={{bg:"teal.500"}}  h="30vh" w="30vh" justify="flex-end">
-            <Icon color="white" p="4" w="24" h="24" as={ImGithub} />
-            <Text color="gray.800" p="4" fontSize="xl" fontWeight="semibold">
-             Github
-            </Text>
+          <Flex rounded="xl" 
+         direction="column" 
+         mt={4} 
+         bg="blue.300"
+         justify="flex-end"
+         ml={isNotSmallerScreen ? 4:0}
+         _hover={{bg:"blue.400"}}  h="30vh" w="30vh" >
+          <Icon color="white" p="4" w="24" h="24" as={ImGithub} />
+          <Text color="white" p="4" fontSize="xl" fontWeight="semibold">
+           Github
+          </Text>
           </Flex>
         </Flex>
       </Box>
